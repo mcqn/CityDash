f8ecc6860d8c606669a9a555d26a96bccc462c19
Adrian McEwen <adrianm@mcqn.com>
2017-02-18 20:27:34 +0000

    Updates for new Things Network servers, rather than staging.

--- CityDashServer/citydash.rb
+++ CityDashServer/citydash.rb
@@ -1,3 +1,4 @@
+#!/usr/local/bin/ruby
 require 'mqtt'
 require 'json'
 require 'base64'
@@ -18,12 +19,11 @@ else
   exit
 end
 
-def publish_ttn_message(mqtt_client, app_id, dev_id, payload, ttl)
+def publish_ttn_message(mqtt_client, app_id, dev_id, payload)
   # Need to base64 encode the payload
   # Build up parameters for the TTN downlink
   params = {
-    "payload" => Base64.encode64(payload).strip,
-    "ttl" => ttl,
+    "payload_raw" => Base64.encode64(payload).strip,
     "port" => 1
   }
   # Send the message
@@ -33,15 +33,15 @@ puts params.to_json
 end
 
 
-MQTT::Client.connect("mqtts://#{settings['ttn']['user']}:#{settings['ttn']['pass']}@staging.thethingsnetwork.org") do |c|
+MQTT::Client.connect("mqtt://#{settings['ttn']['user']}:#{settings['ttn']['pass']}@#{settings['ttn']['handler']}.thethings.network") do |c|
   c.get('#') do |topic, message|
     puts "#{topic}: #{message}"
     if topic.match(/up$/)
       # We've got a message we care about
       data = JSON.parse(message)
-      puts data['payload']
-      devID = data['dev_eui']
-      status = Base64.decode64(data['payload'])
+      puts data['payload_raw']
+      devID = data['dev_id']
+      status = Base64.decode64(data['payload_raw'])
 
       # Find a github issue with that tag
       url_params = "?state=open&labels=#{devID}"
@@ -49,7 +49,7 @@ MQTT::Client.connect("mqtts://#{settings['ttn']['user']}:#{settings['ttn']['pass
         issue_uri = URI.parse(settings["issues"]["url"]+url_params)
         issue_http = Net::HTTP.new(issue_uri.host, issue_uri.port)
         issue_http.use_ssl = true
-        issue_req = Net::HTTP::Get.new(issue_uri.request_uri, {'User-Agent' => "CityDash/1.0"})
+        issue_req = Net::HTTP::Get.new(issue_uri.request_uri, {'User-Agent' => "CityDash/1.1"})
         issue_data = issue_http.request(issue_req)
       else
         issue_data = Net::HTTP.get_response(URI.parse(settings["issues"]["url"]+url_params))
@@ -79,25 +79,25 @@ MQTT::Client.connect("mqtts://#{settings['ttn']['user']}:#{settings['ttn']['pass
           end
           if issue_data.code.to_i >= 200 && issue_data.code.to_i < 300
             # Issue created okay, mark it as a fault
-            publish_ttn_message(c, settings['ttn']['user'], devID, kCityDashStatusFault, "1h")
+            publish_ttn_message(c, settings['ttn']['user'], devID, kCityDashStatusFault)
           else
             puts "Problem creating issue: #{issue_data.code} #{issue_data.message}: #{issue_data.body}"
           end
         else
           # It's already been reported
           puts "Already an issue #{issues[0]}"
-          publish_ttn_message(c, settings['ttn']['user'], devID, kCityDashStatusFault, "1h")
+          publish_ttn_message(c, settings['ttn']['user'], devID, kCityDashStatusFault)
           # FIXME Add a nudge comment
         end
       else
         if issues.empty?
           # Either the device thinks everything's okay and it is, or
           # the device thinks there's a fault but it's been cleared
-          publish_ttn_message(c, settings['ttn']['user'], devID, kCityDashStatusOk, "1h")
+          publish_ttn_message(c, settings['ttn']['user'], devID, kCityDashStatusOk)
         else
           # Either the device thinks everything's fine but it isn't, or
           # the device knows there's a fault and it hasn't been fixed
-          publish_ttn_message(c, settings['ttn']['user'], devID, kCityDashStatusFault, "1h")
+          publish_ttn_message(c, settings['ttn']['user'], devID, kCityDashStatusFault)
         end
       end
     end
